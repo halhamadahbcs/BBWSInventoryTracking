78a47087dd6182858b2cdf2a9a8ca3eb61701ac9
Hadeel Alhamadah <halhamadah@BondConsultingServices.com>
2022-10-28 14:24:03 -0700

    Get container & location from service task instead of WO

--- BBWS Inventory Tracking/CreateNewInventoryTracking.cs
+++ BBWS Inventory Tracking/CreateNewInventoryTracking.cs
@@ -20,8 +20,8 @@ namespace BBWS_Inventory_Tracking
 
             Entity ServiceTaskInput = context.InputParameters["Target"] as Entity;
 
-            if (!ServiceTaskInput.Contains("msdyn_percentcomplete")) { return;}
-            if (ServiceTaskInput["msdyn_percentcomplete"].ToString() != "100") { return;}
+            if (!ServiceTaskInput.Contains("msdyn_percentcomplete")) { return; }
+            if (ServiceTaskInput["msdyn_percentcomplete"].ToString() != "100") { return; }
 
 
             string fetchXMLServiceTask = @"<fetch top='1'>" +
@@ -33,7 +33,7 @@ namespace BBWS_Inventory_Tracking
                       "</fetch>";
 
             Entity ServiceTask = service.RetrieveMultiple(new FetchExpression(fetchXMLServiceTask)).Entities[0];
-            if (!ServiceTask.Contains("msdyn_tasktype")) { return;}
+            if (!ServiceTask.Contains("msdyn_tasktype")) { return; }
 
             if (ServiceTask.Contains("msdyn_workorder"))
             {
@@ -52,18 +52,17 @@ namespace BBWS_Inventory_Tracking
 
                 //if (FullWorkOrder.Contains("bcs_containerserialno"))
                 if (ServiceTask.Contains("bcs_containerno"))
-                    {
-                        Entity InventoryTrackingEntity = new Entity("bcs_inventorytracking");
+                {
+                    Entity InventoryTrackingEntity = new Entity("bcs_inventorytracking");
 
                     DateTime utc = DateTime.UtcNow;
-                    //
                     TimeZoneInfo mountainZone = TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time");
                     DateTime mountainTime = TimeZoneInfo.ConvertTimeFromUtc(utc, mountainZone);
 
                     InventoryTrackingEntity["bcs_transactiondate"] = mountainTime;
 
 
-                    if (((EntityReference)ServiceTask["msdyn_tasktype"]).Id.ToString().ToUpper() =="F284F93E-5AB7-EC11-983F-000D3A5ACF6C")//pickup
+                    if (((EntityReference)ServiceTask["msdyn_tasktype"]).Id.ToString().ToUpper() == "F284F93E-5AB7-EC11-983F-000D3A5ACF6C")//pickup
                     {
                         InventoryTrackingEntity["bcs_currentlocation"] = new EntityReference("bcs_location", new Guid("58A4958A-C839-ED11-9DB0-000D3A5C23EA"));//truck lcoation
 
@@ -71,15 +70,18 @@ namespace BBWS_Inventory_Tracking
 
                     else if (((EntityReference)ServiceTask["msdyn_tasktype"]).Id.ToString().ToUpper() == "3716E5B7-56BF-EC11-983E-0022480ABBD6")//dropoff
                     {
-                        if (FullWorkOrder.Contains("bcs_location"))
+                        //if (FullWorkOrder.Contains("bcs_location"))
+                        if (ServiceTask.Contains("bcs_location"))
                         {
-                            InventoryTrackingEntity["bcs_currentlocation"] = FullWorkOrder["bcs_location"];
+                            //InventoryTrackingEntity["bcs_currentlocation"] = FullWorkOrder["bcs_location"];
+                            InventoryTrackingEntity["bcs_currentlocation"] = ServiceTask["bcs_location"];
                         }
                     }
 
                     //InventoryTrackingEntity["bcs_containerserialno"] = FullWorkOrder["bcs_containerserialno"];
                     //EntityReference container = (EntityReference)FullWorkOrder["bcs_containerserialno"];
                     InventoryTrackingEntity["bcs_containerserialno"] = ServiceTask["bcs_containerno"];
+
                     EntityReference container = (EntityReference)ServiceTask["bcs_containerno"];
 
 
@@ -109,6 +111,24 @@ namespace BBWS_Inventory_Tracking
                         }
                     }
                     InventoryTrackingEntity["bcs_iscurrent"] = true;
+
+                    string fetchXMLContainer = @"<fetch>" +
+                        "<entity name = 'bcs_container'>" +
+                        "<attribute name='bcs_name'/>" +
+                        "<filter>" +
+                        "<condition attribute = 'bcs_containerid' operator= 'eq' value ='" + container.Id + "'/>" +
+                        "</filter>" +
+                        "</entity>" +
+                        "</fetch>";
+
+                    EntityCollection Containers = service.RetrieveMultiple(new FetchExpression(fetchXMLContainer));
+
+                    if (Containers.Entities.Count > 0)
+                    {
+                        InventoryTrackingEntity["bcs_name"] = Containers.Entities[0]["bcs_name"];
+                    }
+
+
                     service.Create(InventoryTrackingEntity);
                 }
             }
