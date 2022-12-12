bcc1bf6ca0be60a38fda76e121e2fb0b0a772be1
Hadeel Alhamadah <halhamadah@BondConsultingServices.com>
2022-12-12 09:10:47 -0800

    Test1212

--- BBWS Inventory Tracking/CreateNewInventoryTracking.cs
+++ BBWS Inventory Tracking/CreateNewInventoryTracking.cs
@@ -8,6 +8,7 @@ namespace BBWS_Inventory_Tracking
     {
         public void Execute(IServiceProvider serviceProvider)
         {
+            //
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             IOrganizationService service = factory.CreateOrganizationService(context.UserId);
